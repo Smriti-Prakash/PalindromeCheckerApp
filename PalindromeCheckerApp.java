943b2bd284b20430afbe204cee6293d7591d4618
Smriti-Prakash <smriti.prakash012@gmail.com>
2026-02-26 15:50:22 +0530

    UC3:String Reverse

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,30 +1,36 @@
 /**
- * Use Case 2 - Hardcoded Palindrome Check
- * Objective: Check whether a hardcoded string is a palindrome
+ * Use Case 3 - Palindrome Check Using String Reverse
+ * Objective: Reverse a string using loop and compare with original
  */
+
 
 public class PalindromeCheckerApp {
     public static void main(String[] args){
 
-        public static void main(String[] args) {
+        // Original String
+        String original = "level";
 
+        // Reversed String (empty initially)
+        String reversed = "";
 
-            String word = "madam";
-
-            String reversedWord = "";
-
-            for (int i = word.length() - 1; i >= 0; i--) {
-                reversedWord = reversedWord + word.charAt(i);
-            }
-
-            if (word.equals(reversedWord)) {
-                System.out.println("The word \"" + word + "\" is a Palindrome.");
-            } else {
-                System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
-            }
-
-            System.out.println("Program executed successfully.");
+        // Loop to reverse the string
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);  // String concatenation
         }
+
+        // Display reversed string
+        System.out.println("Original String : " + original);
+        System.out.println("Reversed String : " + reversed);
+
+        // Compare using equals() method
+        if (original.equals(reversed)) {
+            System.out.println("Result: The given string is a Palindrome.");
+        } else {
+            System.out.println("Result: The given string is NOT a Palindrome.");
+        }
+
+        System.out.println("Program executed successfully.");
+
     }
 
 }
